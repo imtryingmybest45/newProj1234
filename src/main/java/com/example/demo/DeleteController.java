6d462ffa0f83394936fafb05b91d3959408f1245
unknown <sebokapril@gmail.com>
2025-12-14 16:56:05 -0500

    dlsflkdfl

--- src/main/java/com/example/demo/DeleteController.java
+++ src/main/java/com/example/demo/DeleteController.java
@@ -4,14 +4,16 @@ import org.kohsuke.github.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @RestController
 public class DeleteController {
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000","https://delightful-mushroom-0b98f760f.3.azurestaticapps.net/","https://www.aprilshorrorcorner.com","https://aprilshorrorcorner.com"})
     @PostMapping("/deleteEndpoint")
     public String deleteData(@RequestBody String movieName) throws IOException {
-        String nextString = movieName.substring(0, movieName.length()-1);
-        String newName = nextString.replace("+"," ");
+        String nextString = movieName.substring(0, movieName.length() - 1);
+        String newName = nextString.replace("+", " ");
 
         OtherFunctions otherFunctions = new OtherFunctions();
         String repoName = "frontEndAppCode";
@@ -36,6 +38,20 @@ public class DeleteController {
                 .branch(branch)
                 .commit();
 
+        String filePath2 = "src/pages/" + newName + ".js";
+        Timer timer = new Timer(); // Create a Timer object
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    GHContent hello = repo.getFileContent(filePath2, branch);
+                    GHContentUpdateResponse deleteResponse = hello.delete("wow how are you");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        timer.schedule(task,150000);
         return "Successfully deleted "+newName+". Please wait a few moments for the website to refresh.";
     }
 }
