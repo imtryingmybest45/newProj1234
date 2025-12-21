9799a8659de9a39b6b328ff2bed7615d905535f4
unknown <sebokapril@gmail.com>
2025-12-20 20:45:09 -0500

    changing constants

--- src/main/java/com/example/demo/Constants.java
+++ src/main/java/com/example/demo/Constants.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class Constants{
     public static final String repoOwner = "imtryingmybest45";
     public static final String gitToken = System.getenv("HEDGEHOG");
-    public static final String repoName = "testFrontEndCode";
+    public static final String repoName = "frontEndAppCode";
     public static final String branch = "main";
-    public static final int timeDelay = 15000;
+    public static final int timeDelay = 120000;
 }
